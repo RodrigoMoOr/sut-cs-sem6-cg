cfcebfa4e659f6358d1fbfcf28e74aaf446600f7
Mateusz Grabowski <mateuszgrabowski25@gmail.com>
2022-08-30 16:39:50 +0200

    Changed floors, changed teleportes

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,19 +1,28 @@
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour, IPlayerTriggerable
 {
-
     [SerializeField] int sceneToLoad = -1;
     [SerializeField] Transform spawnPoint;
     [SerializeField] int amountOfPointsToGo = -1;
+    bool isLoading = false;
+    Fader fader;
+    PlayerController player;
+    private void Start()
+    {
+       fader =  FindObjectOfType<Fader>();
+    }
     public void OnTrigger(PlayerController player)
     {
-        if(amountOfPointsToGo <= PlayerStats.Instance.ECTS)
+        this.player = player;
+        if ((amountOfPointsToGo <= PlayerStats.Instance.ECTS) && !isLoading)
+        { 
+            isLoading = !isLoading;
             StartCoroutine(SwitchScene(player));
+        }
         else Debug.Log("Not enough ECTS");
     }
 
@@ -21,11 +30,20 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
     IEnumerator SwitchScene(PlayerController player)
     {
         DontDestroyOnLoad(gameObject);
-        var destination  = FindObjectsOfType<Portal>().First(x=>x!=this);
-        player.transform.position = destination.spawnPoint.position;
-
+        yield return fader.FadeIn(0.5f);
+        Debug.Log("dupaaaa");
         yield return SceneManager.LoadSceneAsync(sceneToLoad);
+        
+        var destination  = FindObjectsOfType<Portal>().First(x=>x!=this);
+        player.transform.position = destination.SpawnPoint.position;
 
+        yield return fader.FadeOut(0.5f);
+
+
+
+        isLoading = !isLoading;
         Destroy(gameObject);
     }
+
+    public Transform SpawnPoint => spawnPoint;
 }
